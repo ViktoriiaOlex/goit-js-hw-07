8010e0118b933654b3f80c32d89f85041b7d0dd1
ViktoriiaOlex <slavika86@gmail.com>
2023-12-22 10:25:35 +0200

    add task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,15 @@
+const registerForm = document.querySelector(".login-form");
+
+registerForm.addEventListener("submit", event => {
+    event.preventDefault();
+    const userData = {
+        email: event.target.elements.email.value.trim(),
+        password: event.target.elements.password.value.trim(),
+    };
+    if (userData.email === "" || data.password === "") {
+        return alert("All form fields must be filled in");
+    }
+    console.log(userData);
+    event.target.reset();
+});
 
